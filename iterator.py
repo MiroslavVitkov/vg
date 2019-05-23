d0d9ceed637be30449b9b515891e67ae25f7a78b
Miroslav Vitkov <sir.vorac@gmail.com>
2019-05-23 08:09:51 +0200

    [iter] I believe the iterators are usable now.
    Problems:
    - Retrieving the scene graph is extremely slow, as would be retrieving of the actual image.
        A lazy evaluation mechanism needs to be implemented to enable efficient generic access.
    
    - Python's comparrison of objects requires them to implement at the very least
        __eq__() and __ne__(). Perhaps compare the dicts?

--- iterator.py
+++ iterator.py
@@ -6,13 +6,15 @@ This module provides two iterators over the Visual Genome data.
 
 The remote iterator works over http.
 The local iterator caches about 2.3GB of metadata,
-but image pixel data and scene graphs are still retrieved via http.  # TODO: why?
+but image pixel data and scene graphs are still retrieved via http.
+because of their large size, the underlying library doesn't support
+caching them.
 '''
 
 
-from abc import abstractmethod
 from io import BytesIO
 from os.path import isfile, isdir
+import pickle
 from PIL import Image
 import urllib
 from visual_genome import api as vgr
@@ -20,59 +22,73 @@ from visual_genome import local as vgl
 from zipfile import ZipFile
 
 
-__all__ = ['Remote Local']  # TODO: this doesn't work
-
-
 class Iter:
-    '''
-    ids: [int] A list of image ids. None means all.
-    returns: [tuple] (image metadata, region descriptions, scene graph).  # TODO: get a list of region graphs instead!
-    '''
     def __init__(me, ids=None):
-        super().__init__(ids)
+        '''ids: [int] A list of image ids. None means all.'''
+        raise NotImplementedError('This is an abstract base class.'
+                                  'Please use on of the derived classes instead.')
 
-    @abstractmethod
+
+    def get_all_image_ids(me):
+        '''returns: [list] the ids of all images in the visual genome.'''
+
+
     def __iter__(me):
-        pass
+        '''returns: [tuple] (image metadata, regions in image, scene graph).'''
 
 
 class Remote(Iter):
     def __init__(me, ids):
         if ids is None:
-            ids = vgr.get_all_image_ids()
+            ids = me.get_all_image_ids()
         me.ids = ids
+
+
+    @staticmethod
+    def get_all_image_ids():
+        return vgr.get_all_image_ids()
 
 
     def __iter__(me):
         for id in me.ids:
             regions = vgr.get_region_descriptions_of_image(id)
             image = regions[0].image
-            graph = vgr.get_scene_graph_of_image(id)  # slow
+ #           graph = vgr.get_scene_graph_of_image(id)  # slow
+            graph = None
             yield image, regions, graph
 
 
 class Local(Iter):
-    '''data_dir: [string] Gets created and overwritten with 2.3GB of cached data.'''
     def __init__(me, ids, data_dir='./data/'):
+        '''data_dir: [string] Gets created and overwritten with 2.3GB of cached data.'''
         me.data_dir = data_dir
         me.download_dataset(me.data_dir)
 
         all_regions = vgl.get_all_region_descriptions(data_dir)  # slow
-        me.all_regions = {r[0].image.id: r for r in all_regions}
+        # r here is the list of all regions in one image
+        me.regions = {r[0].image.id: r
+                      for r in all_regions
+                      if r[0].image.id in ids}
 
         if ids is None:
-            ids = vgr.get_all_image_ids()
+            ids = me.get_all_image_ids()
         me.ids = ids
+
+
+    def get_all_image_ids(me):
+        with open(me.data_dir+'/all_image_ids', 'rb') as f:
+            return pickle.load(f)
 
 
     def __iter__(me):
         for id in me.ids:
-            regions = me.all_regions[id]
+            regions = me.regions[id]
             image = regions[0].image
-            graph = vgl.get_scene_graph(id
-                                       ,images=me.data_dir
-                                       ,image_data_dir=me.data_dir+'/by-id/'
-                                       ,synset_file=me.data_dir+'/synsets.json')
+#            graph = vgl.get_scene_graph(id
+#                                       ,images=me.data_dir
+#                                       ,image_data_dir=me.data_dir+'/by-id/'
+#                                       ,synset_file=me.data_dir+'/synsets.json')
+            graph = None
             yield image, regions, graph
 
 
@@ -97,17 +113,45 @@ class Local(Iter):
         if not isdir(path+'/by-id'):
             vgl.save_scene_graphs_by_id(data_dir=path, image_data_dir=path+'/by-id/')
         get('synsets')
+        if not isfile(path+'/all_image_ids'):
+            ids = Remote.get_all_image_ids()
+            with open(path+'/all_image_ids', 'wb') as f:
+                pickle.dump(ids, f)
 
 
+# TODO: split this horror into profile() and verify()
+# TODO: figure out python's object comparrison rules
+# perhaps do all(a == b for a, b in izip_longest(gen_1, gen_2, fillvalue=sentinel))
 def main():
     from random import sample
-    all_ids = vgr.get_all_image_ids()  # TODO: picle to a file
-    ids = sample(all_ids, 20)
+    from time import time
+    points = 1
+
+    all_ids = Remote.get_all_image_ids()
+    ids = sample(all_ids, points)
 
     remote = Remote(ids)
     local = Local(ids)
-    for r, l  in zip(remote, local):
-        assert r == l
+
+    start = time()
+    rem = list(remote)
+    print('Time to read one record remotely:', (time()-start)/points, 'seconds.')
+
+    start = time()
+    loc = list(local)
+    print('Time to read one record locally:', (time()-start)/points, 'seconds.')
+
+    for r, l  in zip(rem, loc):
+        print('========================== r0 =============================')
+        print(r[0])
+        print('========================== l0 =============================')
+        print(l[0])
+        assert r[0].id == l[0].id
+        assert r[0].coco_id == l[0].coco_id
+        assert r[0].id == l[0].id
+        assert r[0].id == l[0].id
+#        assert r[1] == l[1]
+#        assert r[2] == l[2]
 
 
 if __name__ == '__main__':
